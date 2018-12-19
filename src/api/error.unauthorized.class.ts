c998948471b34a0dddf44c7448bdf9952c8e61dc
Aitor Guerrero <aguerrerolsasarte@gmail.com>
2018-12-19 11:41:06 +0100

    optional message in ApiRequestUnauthorizedError

--- src/api/error.unauthorized.class.ts
+++ src/api/error.unauthorized.class.ts
@@ -3,7 +3,10 @@ import {ApiRequestError} from "./error.api-request.class";
 export class ApiRequestUnauthorizedError extends ApiRequestError {
 	public readonly statusCode: number = 401;
 
-	public constructor() {
-		super("unauthorized", "You are not authorized to access this resource");
+	public constructor(message?: string) {
+		super(
+			"unauthorized",
+			message || "You are not authorized to access this resource",
+		);
 	}
 }
