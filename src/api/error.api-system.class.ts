d2e65d93583cb73ca26b748c7aeb554d851492c4
Aitor Guerrero <aguerrerolsasarte@gmail.com>
2019-01-24 17:34:46 +0100

    feature: api system error

---
+++ src/api/error.api-system.class.ts
@@ -0,0 +1,9 @@
+export class ApiSystemError extends Error {
+
+	public readonly statusCode: number = 500;
+
+	constructor(code: string, message?: string) {
+		super(message);
+		this.name = code;
+	}
+}
