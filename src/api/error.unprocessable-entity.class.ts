9f23caef47cebe78893bd06fd7b662e208a2dfda
Aitor Guerrero <aguerrerolsasarte@gmail.com>
2019-01-24 17:35:59 +0100

    feature: api unprocessable entity error

---
+++ src/api/error.unprocessable-entity.class.ts
@@ -0,0 +1,13 @@
+import {ApiRequestError} from "./error.api-request.class";
+
+export class ApiUnprocessableEntityError extends ApiRequestError {
+
+	public readonly statusCode: number = 422;
+
+	public constructor(message?: string) {
+		super(
+			"unauthorized",
+			message || "You are not authorized to access this resource",
+		);
+	}
+}
