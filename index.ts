da4f936af03d23b70f7b6b1384df8b2223aa5a7c
Aitor Guerrero <aitor.guerrero@worldcoo.com>
2019-11-08 12:31:28 +0100

    added callback types to export

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
-import { decorateHandler, ICallbacks as IDecoratorHandlerCallbacks } from "./src/decorator.handler";
-import { decorateHandlerWithCallbacks, ICallbacks } from "./src/decorator.handler-callbacks";
+/* tslint:disable:import-spacing */
+import {decorateHandler, ICallbacks as IDecoratorHandlerCallbacks} from "./src/decorator.handler";
+import {decorateHandlerWithCallbacks, ErrorCallback, ICallbacks, PostCallback, PreCallback}
+	from "./src/decorator.handler-callbacks";
 import decorateHandlerWithCustomError from "./src/decorator.handler-custom-error";
-import { decorateHandlerWithErrorMiddleware } from "./src/decorator.handler-error-middleware";
+import {decorateHandlerWithErrorMiddleware} from "./src/decorator.handler-error-middleware";
 import decorateHandlerWithLifeCycleEventsEmitter from "./src/decorator.handler-life-cycle-events-emissor";
-import { decorateHandlerWithOutputMiddleware } from "./src/decorator.handler-response-middleware";
+import {decorateHandlerWithOutputMiddleware} from "./src/decorator.handler-response-middleware";
 import decorateHandlerWithTimeoutControl from "./src/decorator.handler-timeout-control";
 import EventError from "./src/event.error.class";
 import EventFlushed from "./src/event.flushed.class";
@@ -12,15 +14,15 @@ import EventPersisted from "./src/event.persisted.class";
 import EventSuccess from "./src/event.success.class";
 import EventTimeout from "./src/event.timeout.class";
 import decorateHttpApiHandlerWithHttpApiLogic from "./src/http-api/decorator.api-handler";
-import { ApiRequestError } from "./src/http-api/error.api-request.class";
-import { ApiSystemError } from "./src/http-api/error.api-system.class";
-import { ApiRequestNotFoundError } from "./src/http-api/error.not-found.class";
-import { ApiRequestUnauthorizedError } from "./src/http-api/error.unauthorized.class";
-import { ApiUnprocessableEntityError } from "./src/http-api/error.unprocessable-entity.class";
+import {ApiRequestError} from "./src/http-api/error.api-request.class";
+import {ApiSystemError} from "./src/http-api/error.api-system.class";
+import {ApiRequestNotFoundError} from "./src/http-api/error.not-found.class";
+import {ApiRequestUnauthorizedError} from "./src/http-api/error.unauthorized.class";
+import {ApiUnprocessableEntityError} from "./src/http-api/error.unprocessable-entity.class";
 import EventApiSuccessSuccess from "./src/http-api/event.api-success.class";
-import { buildHttpApiLambdaProxyHandler } from "./src/http-api/proxy-handler-factory.class";
-import buildFifoConsumerHandler,
-	{ ICallbacks as IFifoQueueConsumerHandlerCallbacks } from "./src/queue-consumer/fifo-queue-consumer-handler.functor";
+import {buildHttpApiLambdaProxyHandler} from "./src/http-api/proxy-handler-factory.class";
+import buildFifoConsumerHandler, {ICallbacks as IFifoQueueConsumerHandlerCallbacks}
+	from "./src/queue-consumer/fifo-queue-consumer-handler.functor";
 
 export {
 	decorateHandler,
@@ -48,4 +50,7 @@ export {
 	IDecoratorHandlerCallbacks,
 	IFifoQueueConsumerHandlerCallbacks,
 	ICallbacks,
+	PreCallback,
+	PostCallback,
+	ErrorCallback,
 };
