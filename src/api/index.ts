ad0007f4a9243473832dd5ae338405208ebac713
Aitor Guerrero <aitgud.dev@gmail.com>
2021-10-28 14:14:24 +0200

    added api errors to exports

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,10 @@ import Callbacks from './callbacks.class';
 
 export * from './handler-factory.class';
 export * from './error.api-request.class';
+export * from './error.api-system.class';
+export * from './error.not-found.class';
 export * from './error.unauthorized.class';
+export * from './error.unprocessable-entity.class';
 export * from './output.interface';
 export * from './proxy-handler-factory.class';
 export * from './multi-handler-factory.class';
